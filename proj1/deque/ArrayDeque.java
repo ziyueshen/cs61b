1c9d409009885b7cc69c3be3d92b9d0332ca8906
Ziyue Shen <ziyueshen@126.com>
2023-10-27 21:58:05 -0400

    done with checkpoint

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,2 +1,109 @@
-package deque;public class ArrayDeque {
+package deque;
+
+public class ArrayDeque<AnyThing> {
+    private AnyThing[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    /** Creates an empty list. */
+    public ArrayDeque() {
+        items = (AnyThing []) new Object[8];
+        size = 0;
+        nextFirst = 4;
+        nextLast = 5;
+    }
+    public boolean isEmpty() {
+        if ( size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Inserts X into the back of the list. */
+    public void addLast(AnyThing x) {
+        if (nextLast < items.length) {
+            items[nextLast] = x;
+            nextLast += 1;
+        } else {
+            items[0] = x;
+            nextLast = 1;
+        }
+        size += 1;
+    }
+    /** Inserts X into the front of the list. */
+    public void addFirst(AnyThing x) {
+        if (nextFirst >= 0) {
+            items[nextFirst] = x;
+            nextFirst -= 1;
+        } else {
+            items[items.length - 1] = x;
+            nextFirst = items.length - 2;
+        }
+        size += 1;
+    }
+
+    /** Gets the ith item in the list (0 is the front). */
+    public AnyThing get(int index) {
+        return items[index];
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return size;
+    }
+
+    /** Deletes item from back of the list and
+     * returns deleted item. */
+    public AnyThing removeLast() {
+        AnyThing lstItem;
+        size -= 1;
+        if (nextLast == 0) {
+            lstItem = items[items.length - 1];
+            items[items.length - 1] = null;
+            nextLast = items.length - 1;
+        } else {
+            lstItem = items[nextLast - 1];
+            items[nextLast - 1] = null;
+            nextLast -= 1;
+        }
+        return lstItem;
+    }
+    /** Deletes item from front of the list and
+     * returns deleted item. */
+    public AnyThing removeFirst() {
+        size -= 1;
+        AnyThing fstItem;
+        if (nextFirst == items.length - 1) {
+            fstItem = items[0];
+            items[0] = null;
+            nextFirst = 0;
+        } else {
+            fstItem = items[nextFirst + 1];
+            items[nextFirst + 1] = null;
+            nextFirst += 1;
+        }
+        return fstItem;
+    }
+
+    /** Prints the items in the deque from first to last, separated by a space.
+     * Once all the items have been printed, print out a new line.*/
+    public void printDeque() {
+        int keepTrack = size;
+        int startPrint;
+        if (nextFirst == items.length - 1) {
+            startPrint = 0;
+        } else {
+            startPrint = nextFirst + 1;
+        }
+        while(keepTrack > 0) {
+            keepTrack -= 1;
+            System.out.print(items[startPrint]);
+            if (startPrint == items.length - 1) {
+                startPrint = 0;
+            } else {
+                startPrint = nextFirst + 1;
+            }
+            startPrint += 1;
+        }
+    }
 }
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,2 +1,125 @@
-package deque;public class LinkedListDeque {
+package deque;
+/** A double ended queue.Use double sentinel.*/
+public class LinkedListDeque<AnyThing> {
+    /** The structure underneath the Deque.*/
+    private class AnyNode {
+        private AnyThing item;
+        private AnyNode next;
+        private AnyNode prev;  // Circular structure.
+
+        private AnyNode(AnyThing i, AnyNode n, AnyNode p) {  //Constructor, like__init__.
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+    // AnyNode first;
+    AnyNode sentinel;
+    int size;
+    /** Create an empty list*/
+    public LinkedListDeque() {  // Two ways to instantiate, 0 element or 1 element.
+        sentinel = new AnyNode(null, null, null);  // Use null as the initial value, cause later it could be reassigned to any type
+        sentinel.next =sentinel;
+        sentinel.prev =sentinel;
+        size = 0;
+    }
+    public LinkedListDeque(AnyThing x) {  //To instantiate, pass in an item.
+        // first = new AnyNode(x, null);  // Note: first is the body of LinkedListDeque object; first.item = x; first.next
+        sentinel = new AnyNode(null, null, null);
+        sentinel.next = new AnyNode(x, sentinel, sentinel);
+        sentinel.prev = new AnyNode(x, sentinel, sentinel);
+        size = 1;
+    }
+    /** Add an element to the front.*/
+    public void addFirst(AnyThing x) {
+        // first = new AnyNode(x, first); //Important! Must create a new AnyNode.
+        AnyNode newNode = new AnyNode(x, sentinel.next, sentinel);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;  // Never mutate sentinel itself.
+        size += 1;
+    }
+    /** Add an element to the back.*/
+    public void addLast(AnyThing x) {
+        size += 1;
+        AnyNode newNode = new AnyNode(x, sentinel, sentinel.prev);
+        sentinel.prev.next =  newNode;
+        sentinel.prev = newNode;
+        /*
+        AnyNode p = first;
+        // Move p until it reaches the end of the list.
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new AnyNode(x, null);
+        */
+    }
+    /** Removes and returns the item at the front of the deque. If no such item exists, returns null.*/
+    public AnyThing removeFirst() {
+        if (size > 0) {
+            size -= 1;
+        }
+        AnyThing fstItem = sentinel.next.item;
+        sentinel.next.next.prev = sentinel;
+        sentinel.next = sentinel.next.next;
+        return fstItem;
+    }
+    /** Removes and returns the item at the back of the deque. If no such item exists, returns null.*/
+    public AnyThing removeLast() {
+        if (size > 0) {
+            size -= 1;
+        }
+        AnyThing lstItem = sentinel.prev.item;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        return lstItem;
+    }
+    /** Returns true if deque is empty, false otherwise.*/
+    public boolean isEmpty() {
+        if (sentinel.next.item == null) {
+            return true;
+        }
+        return false;
+    }
+    /**  Returns the number of items in the deque.*/
+    public int size() {
+        return size;
+    }
+
+    /** Prints the items in the deque from first to last, separated by a space.
+     * Once all the items have been printed, print out a new line.*/
+    public void printDeque() {
+        AnyNode p = sentinel.next;
+        // Move p until it reaches the end of the list.
+        while (p.next != sentinel) {
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * Must use iteration, not recursion.*/
+    public AnyThing get(int index) {
+        AnyNode p = sentinel.next;
+        int keepTrack = 0;
+        while (keepTrack < index) {
+            p = p.next;
+            keepTrack += 1;
+        }
+        return p.item;
+    }
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * Must use recursion.*/
+    public AnyThing getRecursive(int index) {
+        AnyThing res;
+        res = getHelper(sentinel.next, index);
+        return res;
+    }
+    /** Helper for getRecursive.*/
+    private AnyThing getHelper(AnyNode p, int index) {
+        if (index == 0) {
+            return p.item;
+        } else {
+            return getHelper(p.next, index - 1);
+        }
+    }
 }
